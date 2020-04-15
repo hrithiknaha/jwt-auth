97ba63f9ff27eff591bae768610b37abb0379764
Hrithik Naha <naha.hrithkk@gmail.com>
2020-04-15 12:32:52 +0530

    Add joi, made validation.js and add register and login validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 const User = require('../models/User');
 
+const { registerValidation } = require('./validation');
+
 router.post('/register', async (req, res) => {
+	const { error } = registerValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
 	const user = new User({
 		name: req.body.name,
 		email: req.body.email,
