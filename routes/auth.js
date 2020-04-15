79193d3f0f801cba9ccb56afec2f26512aa92692
Hrithik Naha <naha.hrithkk@gmail.com>
2020-04-15 14:13:29 +0530

    Added path to make it store in cookies

--- routes/auth.js
+++ routes/auth.js
@@ -48,7 +48,7 @@ router.post('/login', async (req, res) => {
 
 		//Create and assign token is login successful
 		const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-		res.header('auth-token', token).send(token);
+		res.header('auth-token', token).send(token); //use res.cookies https://dev.to/mr_cea/remaining-stateless-jwt-cookies-in-node-js-3lle
 	});
 });
 
--- routes/verifyTokens.js
+++ routes/verifyTokens.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 function auth(req, res, next) {
-	const token = req.header('auth-token');
+	const token = req.header('auth-token'); //use req.cookies.token
 	if (!token) return res.status(401).send('Access Denied!');
 
 	try {
